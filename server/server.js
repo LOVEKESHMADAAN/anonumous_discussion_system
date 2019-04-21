d9be658dedb1bd0da8f4e5e51aed1c25186f338b
LOVEKESHMADAAN <lovekeshmadaan@gmail.com>
2019-04-21 19:10:59 +0530

    new changes

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ io.on('connection', (socket) => {
 
     io.to(params.room).emit('updateUserList',users.getUserList(params.room));
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Discussion ! Share your thoughts'));
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
     socket.broadcast.to(params.room).emit('NotifyMeMessage', generateMessage('Admin', `${params.name} has joined`));
     callback();
